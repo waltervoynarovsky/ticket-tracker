d13df5513dd699c0c870cc594535b2ccbadefef9
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-12-02 16:45:05 +0000

    removed boilerplate files

---
+++ ticket-tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="greeting">
+        <h1 className="greeting__heading">Hello World</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
