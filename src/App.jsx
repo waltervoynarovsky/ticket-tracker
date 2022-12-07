4ded7745bd352e54e4ab293f1f8fda4ef7f13c7a
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-12-07 09:18:38 +0000

    slight styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,15 @@ const App = () => {
   // const searchedTickets = team.filter(() => {
   //   const searchedEmployees = team.name.includes(searchTerm);
   //   return searchedEmployees;
-
-  //   const allPeople = searchedTickets.map((person) => {});
   // });
+
   return (
     <div className="app">
       <header className="greeting">
         <h1 className="greeting__heading">Ticket Tracker</h1>
         <SearchBox searchTerm={searchTerm} handleInput={handleInput} />
+        {/* <EmployeeTicketList
+          teamArr={sear.length > 0 ? searchedTickets : team} */}
         <EmployeeTicketList teamArr={team} />
       </header>
     </div>
